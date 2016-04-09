ac3956813e2e9fc3a445bf4151c55ed51463f350
Michael Farrell <michaelmerrittfarrell@gmail.com>
2016-04-09 16:47:36 +0000

    created helper to build url for public key

---
+++ src/js/helpers/urlFor.js
@@ -0,0 +1,11 @@
+import publicKey from './publicKey';
+
+// Build a URL in the format of:
+// http://slackr-api.thefirehoseproject.com/:public_key/:path
+const urlFor = (path) => {
+  return "http://slackr-api.thefirehoseproject.com/" +
+    publicKey() +
+    path;
+}
+
+export default urlFor;
