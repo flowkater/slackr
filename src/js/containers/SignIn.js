152d7d0c3c36b5cad7604ab374512acecd0da11a
Michael <MichaelMerrittFarrell@gmail.com>
2016-04-27 21:17:07 -0400

    created signIn action and bound action to dispatch within SignIn container

--- src/js/containers/SignIn.js
+++ src/js/containers/SignIn.js
@@ -2,9 +2,9 @@ import React from 'react';
 import SignInForm from '../components/SignInForm';
 import {connect} from 'react-redux';
 import getParams from '../helpers/getParams';
-// import {bindActionCreators} from 'redux';
-// import * as Actions from './actions/SignIn.js';
-//
+import signIn from '../actions/signIn';
+import {bindActionCreators} from 'redux';
+
 const mapStateToProps = (state) => {
   return {
     'signin': getParams(state.form.signin, ['email', 'password'])
@@ -12,12 +12,23 @@ const mapStateToProps = (state) => {
 };
 
 @connect(
-  mapStateToProps
+  mapStateToProps,
+  dispatch => ({
+    ... bindActionCreators({signIn}, dispatch)
+  })
 )
 
 export default class SignIn extends React.Component {
   submit(e) {
-    console.log(this.props.signin);
+    const signInAttributes = {
+      user: {
+        email: this.props.signin.email,
+        password: this.props.signin.password
+      }
+    };
+
+    this.props.signIn(signInAttributes);
+
     e.preventDefault();
   }
 
--- src/js/reducers/currentUser.js
+++ src/js/reducers/currentUser.js
@@ -1,6 +1,6 @@
 const currentUser = (state = {}, action) => {
   switch (action.type) {
-    case "LOAD_CURRENT_USER":
+    case 'LOAD_CURRENT_USER':
     return action.payload.data;
     default:
     return state;
